6089a3fcbb389f31d52f707f2d749b3bcc5c2eb5
giraffekey <giraffekey@tutanota.com>
2025-10-04 10:02:25 -0700

    Add parallax background

--- scripts/level.gd
+++ scripts/level.gd
@@ -20,7 +20,7 @@ func _ready() -> void:
 				$Obstacles.add_child(crystal)
 			Vector2i(3, 0):
 				var exit = load("res://scenes/objects/exit.tscn").instantiate()
-				exit.position = $Tiles/ObjectLayer.map_to_local(cell)
+				exit.position = $Tiles/ObjectLayer.map_to_local(cell) + Vector2(8, 8)
 				$Obstacles.add_child(exit)
 			Vector2i(0, 1):
 				var spike = load("res://scenes/objects/spike.tscn").instantiate()
@@ -31,3 +31,24 @@ func _ready() -> void:
 				bat.position = $Tiles/ObjectLayer.map_to_local(cell)
 				$Obstacles.add_child(bat)
 	$Tiles/ObjectLayer.queue_free()
+
+func _process(_delta: float) -> void:
+	var x = 1.0 - $Players/Player.position.x / 2880
+	$Background/Background1a.position.x = x * 640 - 320
+	$Background/Background1b.position.x = x * 640 + 320
+	$Background/Background2a.position.x = fmod(x * 640 * 2, 640) - 320
+	$Background/Background2b.position.x = fmod(x * 640 * 2, 640) + 320
+	$Background/Background3a.position.x = fmod(x * 640 * 3, 640) - 320
+	$Background/Background3b.position.x = fmod(x * 640 * 3, 640) + 320
+	$Background/Background4a.position.x = fmod(x * 640 * 4, 640) - 320
+	$Background/Background4b.position.x = fmod(x * 640 * 4, 640) + 320
+
+	var y = 60 + (1.0 - $Players/Player.position.y / 480) * 120
+	$Background/Background1a.position.y = y
+	$Background/Background1b.position.y = y
+	$Background/Background2a.position.y = y
+	$Background/Background2b.position.y = y
+	$Background/Background3a.position.y = y
+	$Background/Background3b.position.y = y
+	$Background/Background4a.position.y = y
+	$Background/Background4b.position.y = y
